03a49354c82da61d4cd771e6311eb6ed7e719416
AdrianaDillarza <itzel.dillarza@gmail.com>
2017-12-16 10:53:58 -0600

    Actualizando js y css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,45 @@
-/*
- * Funcionalidad de tu producto
- */
+/* ---------------------------------------------------------------
+-----Funcion de Tabuladores*/
+   function mostrarOcultar(event) {
+     var tabSeleccionado = event.target.dataset.tabSeleccionado;
+     var overflow = document.getElementById("overviewTab");
+     var students = document.getElementById("studentsTab");
+     var teachers = document.getElementById("teachersTab");
+     if(tabSeleccionado === "tabOverview"){
+      console.log("overflow");
+      students.style.display = "none";
+      teachers.style.display = "none";
+      //muestra el tab
+      overflow.style.display = "block";
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+     }else if (tabSeleccionado === "tabStudents") {
+    console.log("studen");
+     teachers.style.display = "none";
+     overflow.style.display = "none";
+     //muestra la tab
+     students.style.display = "block";
+
+     }else if (tabSeleccionado === "tabTeachers") {
+    console.log("tecaher");
+     students.style.display = "none";
+     overflow.style.display = "none";
+     //muestra la tab
+     teachers.style.display = "block";
+     }
+
+   }
+     var cargarPagina = function() {
+       var overflow = document.getElementById("overviewTab");
+       var students = document.getElementById("studentsTab");
+       var teachers = document.getElementById("teachersTab");
+       overflow.style.display = "none";
+       students.style.display = "none";
+       teachers.style.display = "none";
+
+       var elementosTab = document.getElementsByClassName("tab");
+       for (var i = 0; i < elementosTab.length; i++) {
+         elementosTab[i].addEventListener("click",mostrarOcultar);
+       }
+     }
+
+   cargarPagina();
